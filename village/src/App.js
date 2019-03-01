e0705877f794f5ad31a8d7861e12f24737e8ca20
Javontay McElroy <dinomightnetwork@gmail.com>
2019-03-01 10:21:32 -0600

    Starting Sprint.

--- village/src/App.js
+++ village/src/App.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
-import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+
+import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
   }
+  async componentDidMount() {
+    try {
+      const res = await axios.get('http://localhost:5000/smurfs');
+      this.setState({ smurfs: res.data });
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <SmurfForm />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
