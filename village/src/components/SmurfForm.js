86730c34876f8610d3ee7a588b225717a2967c06
Javontay McElroy <dinomightnetwork@gmail.com>
2019-03-01 10:39:37 -0600

    Post function working! Going to Routes.

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -12,14 +12,21 @@ class SmurfForm extends Component {
 
   addSmurf = event => {
     event.preventDefault();
-    // add code to create the smurf using the api
+
+    const newSmurf = {
+      name: event.target.name.value,
+      age: Number(event.target.age.value),
+      height: event.target.height.value
+    };
+
+    this.props.handleNewSmurf(newSmurf);
 
     this.setState({
       name: '',
       age: '',
       height: ''
     });
-  }
+  };
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
@@ -27,27 +34,27 @@ class SmurfForm extends Component {
 
   render() {
     return (
-      <div className="SmurfForm">
+      <div className='SmurfForm'>
         <form onSubmit={this.addSmurf}>
           <input
             onChange={this.handleInputChange}
-            placeholder="name"
+            placeholder='name'
             value={this.state.name}
-            name="name"
+            name='name'
           />
           <input
             onChange={this.handleInputChange}
-            placeholder="age"
+            placeholder='age'
             value={this.state.age}
-            name="age"
+            name='age'
           />
           <input
             onChange={this.handleInputChange}
-            placeholder="height"
+            placeholder='height'
             value={this.state.height}
-            name="height"
+            name='height'
           />
-          <button type="submit">Add to the village</button>
+          <button type='submit'>Add to the village</button>
         </form>
       </div>
     );
